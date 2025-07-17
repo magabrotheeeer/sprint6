25a03f4dd9e4f2e44c7ac04b4f446fcb532ab605
Magomed <akhilgov_magomed@inbox.ru>
2025-07-17 10:37:25 +0300

    swapped the logs, added port output for the server startup log

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,9 @@ func main() {
 	logger := log.New(os.Stdout, "SERVER: ", log.LstdFlags)
 	srv := server.NewServer(logger)
 
+	logger.Println("Starting the server :8080")
 	if err := srv.Server.ListenAndServe(); err != nil {
 		logger.Println("error when starting the service")
 	}
-	logger.Println("Starting the server")
 }
 
